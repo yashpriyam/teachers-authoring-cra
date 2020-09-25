bd2692ba4857cf2375d3b3fe6f45997778209730
YASH PRIYAM <yashpriyam@YASHs-MacBook-Air.local>
2020-09-25 16:29:52 +0530

    Updates

---
+++ precache-manifest.9300461dde6bf6936fc825e7b7ee2280.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b6bb3112732139cbde724641247dbfc",
+    "url": "/teachers-authoring-cra/index.html"
+  },
+  {
+    "revision": "809e24f77ac0525d05da",
+    "url": "/teachers-authoring-cra/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "f82fde60980fdbee8547",
+    "url": "/teachers-authoring-cra/static/js/2.0abc535e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/teachers-authoring-cra/static/js/2.0abc535e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "809e24f77ac0525d05da",
+    "url": "/teachers-authoring-cra/static/js/main.df25d8e3.chunk.js"
+  },
+  {
+    "revision": "e3a09629e53ce62a12fe",
+    "url": "/teachers-authoring-cra/static/js/runtime-main.f375e656.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/teachers-authoring-cra/static/media/logo.5d5d9eef.svg"
+  }
+]);
