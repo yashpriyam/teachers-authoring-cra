33d6ec207e33780acc2bb6d48e3c66e86032ca6f
YASH PRIYAM <yashpriyam@YASHs-MacBook-Air.local>
2020-09-27 12:48:10 +0530

    add delete component, considering parent child relation

---
+++ src/deletetask/deleteTask.jsx
@@ -0,0 +1,36 @@
+import React, { useContext } from 'react'
+import { AppStateContext } from '../appState/globalState.context'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+
+const DeleteTask = (props) => {
+    const { indPos } =  props
+    const [ appState, dispatch ] = useContext(AppStateContext)
+    let updatedState = [...appState]
+
+    const deleteTaskWithChildren = () => {
+        const taskObj = updatedState[indPos]
+        let splicingIndex = 1;
+        for (let pos = indPos + 1; pos < updatedState.length; pos += 1) {
+            if (updatedState[pos].level > taskObj.level) {
+                splicingIndex += 1;
+                continue;
+            }
+            break;
+        }
+        updatedState.splice(indPos, splicingIndex)
+        dispatch({ type: 'deleteTask', value: updatedState });
+      }
+
+    return (
+        <div>
+            <FontAwesomeIcon
+                className='arrow'
+                onClick={() => deleteTaskWithChildren()}
+                icon={faTrashAlt}
+            />
+        </div>
+    )
+}
+
+export default DeleteTask
