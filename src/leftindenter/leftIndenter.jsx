e8b4cada53996bfcf61ad07eaa12610a3e737b5c
YASH PRIYAM <yashpriyam@YASHs-MacBook-Air.local>
2020-09-27 12:19:58 +0530

    add task viewer component, left indenter and right indenter

---
+++ src/leftindenter/leftIndenter.jsx
@@ -0,0 +1,30 @@
+import React, { useContext } from 'react'
+import { AppStateContext } from '../appState/globalState.context'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+
+
+const LeftIndenter = (props) => {
+    const { indPos } =  props
+    const [ appState, dispatch ] = useContext(AppStateContext)
+    let updatedState = [...appState]
+
+    const indentLeft = () => {
+        console.log(updatedState);
+        let taskObj = updatedState[indPos];
+        taskObj.level > 1 ? taskObj.level -= 1 : Error('No more space')
+        return dispatch({ type: 'indentLeft', value: updatedState })
+      }
+      
+    return (
+        <div>
+            <FontAwesomeIcon
+                className='arrow'
+                onClick={() => indentLeft()}
+                icon={faArrowLeft}
+            />
+        </div>
+    )
+}
+
+export default LeftIndenter;
