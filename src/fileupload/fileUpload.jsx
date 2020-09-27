d6b4de7d3272daa0fcfa6954126768fda7efced1
YASH PRIYAM <yashpriyam@YASHs-MacBook-Air.local>
2020-09-27 16:07:56 +0530

    used appstate to fix eslint error

--- src/fileupload/fileUpload.jsx
+++ src/fileupload/fileUpload.jsx
@@ -1,21 +1,22 @@
-import React, { useContext } from 'react'
-import { AppStateContext } from '../appState/globalState.context'
+import React, { useContext } from "react"
+import { AppStateContext } from "../appState/globalState.context"
 
 const UploadFile = () => {
-    const [ appState, dispatch ] = useContext(AppStateContext)
-
-    const handleFileUpload = e => {
-        const fileReader = new FileReader();
-        fileReader.readAsText(e.target.files[0], "UTF-8");
-        fileReader.onload = ev => {
-          dispatch({ type: 'jsonFileData', value: JSON.parse(ev.target.result)})
-        };
-      };
-    return (
-        <div>
-            <input type="file" onChange={handleFileUpload} />
-        </div>
-    )
+  const [appState, dispatch] = useContext(AppStateContext)
+  let updatedState = [...appState]
+  const handleFileUpload = (e) => {
+    const fileReader = new FileReader()
+    fileReader.readAsText(e.target.files[0], "UTF-8")
+    fileReader.onload = (ev) => {
+      updatedState = JSON.parse(ev.target.result)
+      dispatch({ type: "jsonFileData", value: updatedState })
+    }
+  }
+  return (
+    <div>
+      <input type="file" onChange={handleFileUpload} />
+    </div>
+  )
 }
 
 export default UploadFile
